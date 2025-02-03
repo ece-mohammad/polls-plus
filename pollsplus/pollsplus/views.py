644d656631513ce5b9dd02e58fc50b9206e2dac0
ece-mohammad <kuro.ece@gmail.com>
2025-02-03 15:16:15 +0200

    Updated login
    Signed-off-by: ece-mohammad <kuro.ece@gmail.com>

--- pollsplus/pollsplus/views.py
+++ pollsplus/pollsplus/views.py
@@ -4,10 +4,11 @@
 from django.contrib.auth import logout
 from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.urls import reverse_lazy
 
-
+@csrf_exempt
 @require_http_methods(["GET", "POST"])
 def custom_logout(request: HttpRequest) -> HttpResponseRedirect:
     next_page: str = request.GET.get("next", "/")
